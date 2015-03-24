739c783c3db09c55c983c816696db914e2b4c886
Alex Baden <alex.baden@gmail.com>
2015-03-23 21:41:51 -0400

    [getdata]: committing before changing request method to post (to stop reset by peer errors)

--- getdata.py
+++ getdata.py
@@ -11,8 +11,9 @@ import time
 import sys
 
 def get_author_pubs(author):
-  author_text = author.split(' ')
-  author_string = "{}, {}[Full Author Name]".format(author_text[1].strip(), author_text[0].strip())
+  #author_text = author.split(' ')
+  #author_string = "{}, {}[Author]".format(author_text[1].strip(), author_text[0].strip())
+  author_string = "{}[Author]".format(author) #.strip(), author_text[0].strip())
   author_term = quote(author_string)
   url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=1000&term={}".format(author_term)
 
@@ -22,7 +23,7 @@ def get_author_pubs(author):
     response = urlopen(request)
     papers = response.read()
   except URLError, e:
-    print "Received error code", e
+    print "Received error code in get pubs", e
 
   return papers
 
@@ -32,23 +33,24 @@ def get_paper_authors(author_raw):
   
   author_json = json.loads(author_raw)
   author_ids = author_json['esearchresult']['idlist']
-
-  for i in xrange(0,len(author_ids),100):
+  print "Getting " + str(len(author_ids)) + " papers"
+  for i in xrange(0,len(author_ids),15):
     start = i
-    end = (i + 99) 
+    end = (i + 14) 
     if end > len(author_ids): 
       end = len(author_ids)
+    print "start: " + str(start) + ", end: " + str(end)
     idlist = ','.join(author_ids[start:end]) 
-  
+    print idlist  
     url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&id={}".format(idlist)
-  
+    print url
     request = Request(url)
       
     try:
       response = urlopen(request)
       abstracts_raw = response.read()
     except URLError, e:
-      print "Received error code", e
+      print "Received error code in get papers", e
     
     abstracts = json.loads(abstracts_raw)
     if 'result' in abstracts:
@@ -61,20 +63,24 @@ def get_paper_authors(author_raw):
 def main():
   # authors[name] = papers (papers is a dict of id => authors on paper)
   authors = {}
+  # departments[name] = department (string) 
+  departments = {}
 
   with open('faculty') as f:
     for line in f:
-      authors[line.strip()] = None
+      tmp = line.split(';')
+      authors[tmp[0].strip()] = tmp[1].strip()
+      departments[tmp[0].strip()] = tmp[2].strip()
 
   for author in sorted(authors.keys()):
     print "Processing ", author
-    paperstmp = get_author_pubs(author) 
-    print paperstmp 
+    paperstmp = get_author_pubs(authors[author]) 
     authors[author] = get_paper_authors(paperstmp)
+    time.sleep(5)
 
   with open('pubmed.pickle', 'w') as f:
-    pickle.dump(authors, f)
-
+    pickle.dump([authors, departments], f)
+    
 if __name__ == '__main__':
   main()
 
